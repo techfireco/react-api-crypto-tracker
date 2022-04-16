76177165bc77edbf6427ec2928bdae81150b0c91
Md Kashif Ali <kashifdot1969@gmail.com>
2022-04-17 02:25:54 +0530

    api-insertion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [coins, setCoins] = useState([])
+  useEffect (() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res =>{
+      setCoins(res.data);
+      console.log(res.data);
+    })
+  })
   return (
     <div className="App">
       <h1>Hello Crypto Tracker</h1>
