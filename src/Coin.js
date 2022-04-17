7415b58bd6d013b160a71ef965fa755977c3cbbe
Md Kashif Ali <kashifdot1969@gmail.com>
2022-04-17 16:00:58 +0530

    Mapping-through-API-data

--- src/Coin.js
+++ src/Coin.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const coin = ({image,name,symbol,price,volume}) => {
+const coin = ({image,name,symbol,price,volume, priceChange, marketcap}) => {
   return (
     <div className='coin-container'>
         <div className='coin-row'>
@@ -12,7 +12,14 @@ const coin = ({image,name,symbol,price,volume}) => {
             <div className="coin-data">
               <p className="coin-price">${price}</p>
               <p className="coin-volume">${volume.toLocaleString()}</p>
-
+              {priceChange<0?(
+              <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
+              ) : (
+              <p className="coin-percent green">{priceChange.toFixed(2)}%</p>)
+              }
+              <p className="coin-marketcap"> 
+              Mkt Cap: ${marketcap.toLocaleString()} 
+              </p>
             </div>
         </div>
     </div>
